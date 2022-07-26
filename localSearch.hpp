8695d99b26ebbb9a7c5fc375ca91c967e449ae37
Lemos <86426372+lemoslemos@users.noreply.github.com>
2022-07-26 08:54:42 -0300

    missing include

--- localSearch.hpp
+++ localSearch.hpp
@@ -1,6 +1,7 @@
 #ifndef LOCAL_SEARCH_H
 #define LOCAL_SEARCH_H
 
+#include "subsequence.hpp"
 #include "solution.hpp"
 
 void localSearch (Solution *s, double **matrixAdj, std::vector<std::vector<Subsequence>>& subseqMatrix);
